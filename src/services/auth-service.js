019c4563fa45bab0f9a829c4ade68412c0153527
Tripoli80 <tripoli80@gmail.com>
2023-03-14 12:35:23 +0200

    first6

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -13,7 +13,7 @@ authService.login = async (email, password) => {
   if (!isMatch) throw new Error("Incorrect password");
 
   const { token, longToken } = await generateToken(user.id);
-  return { token, msg: "tt" };
+  return { token, msg: "tt5" };
 };
 
 authService.register = async (email, password, name) => {
