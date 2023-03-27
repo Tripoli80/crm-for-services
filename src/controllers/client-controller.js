0b408c4918229324e28350f973583478a582f216
Tripoli80 <tripoli80@gmail.com>
2023-03-27 17:51:20 +0300

    ClientAdd-fix

--- src/controllers/client-controller.js
+++ src/controllers/client-controller.js
@@ -7,16 +7,10 @@ export const getClient = async (req, res, next) => {
 };
 
 export const searchClient = async (req, res, next) => {
-  console.log("🚀 ~ file: client-controller.js:11 ~ query:")
   const { query, page } = req.query;
   const data = await clientService.searchClient({ query, page });
   res.status(200).json(data);
 };
-// export const onlineBooking = async (req, res, next) => {
-//   const { firstName, phone } = req.body;
-//   const data = await clientService.checkAndCreate({ firstName, phone });
-//   res.status(201).json(data);
-// };
 export const addClient = async (req, res, next) => {
   const { firstName, phone } = req.body;
   const { client, isNew } = await clientService.createClient({
@@ -38,41 +32,9 @@ export const removeClient = async (req, res, next) => {
   const { deletedClient } = await clientService.delete(id);
   res.status(200).json(deletedClient);
 };
-// {
-//   _id,
-//   firstName,
-//   lastName,
-//   email,
-//   age,
-//   description,
-//   address,
-//   dateOfBirth,
-//   gender,
-//   phone,
-//   customfield,
-// }
 
-// export const updateUser = async (req, res, next) => {
-//   const { user } = req;
-//   const { email, name, age, description, phone, customfield } = req.body;
-//   const data = { _id: user, email, name, age, description, phone, customfield };
-
-//   const updatedUser = await userService.update(data);
-//   res.status(201).json(updatedUser);
-// };
-
-// export const deleteUser = async (req, res, next) => {
-//   const { user } = req;
-//   const removedUser = await userService.delete(user);
-//   if (removedUser)
-//     res.status(200).json({ message: "Successfully removed", removedUser });
-// };
-
-// export const changePassword = async (req, res, next) => {
-//   const { user } = req;
-//   const { password, newPassword } = req.body;
-//   const data = { id: user, password, newPassword };
-
-//   const updatedUser = await userService.changePassword(data);
-//   if (updatedUser) res.status(200).json({ updatedUser });
+// export const onlineBooking = async (req, res, next) => {
+//   const { firstName, phone } = req.body;
+//   const data = await clientService.checkAndCreate({ firstName, phone });
+//   res.status(201).json(data);
 // };
