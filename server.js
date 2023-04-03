02d5cec4dca7015be578eb29e6d41f4e698131a0
Tripoli80 <tripoli80@gmail.com>
2023-04-04 01:33:14 +0300

    coocki [200~3.66.84.144

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from "express";
 // Configure & Run the http server
 const app = express();
 
-app.use(express.static(__dirname, { dotfiles: "allow" }));
+app.use(express.static("./src", { dotfiles: "allow" }));
 
 app.listen(80, () => {
   console.log("HTTP server running on port 80");
