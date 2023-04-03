b8c08ada25f4dcc90cd785bbd2d0c8f547df13b5
Tripoli80 <tripoli80@gmail.com>
2023-04-03 23:01:39 +0300

    cors

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -20,7 +20,7 @@ export const login = async (req, res, next) => {
   });
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
-    domain: ["localhost:3000"],
+    secure: false, // для использования HTTPS
     path: "/",
     sameSite: "none", // разрешить доступ к кукам с других доменов
   });
@@ -59,7 +59,8 @@ export const refreshToken = async (req, res, next) => {
   });
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
-    domain: ["localhost:3000"],
+    secure: false, // для использования HTTPS
+
     path: "/",
     sameSite: "none", // разрешить доступ к кукам с других доменов
   });
